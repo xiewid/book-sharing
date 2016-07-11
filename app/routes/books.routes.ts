0daedc99d2f998be556d3131886766f270d69e4b
Max Widjaja <max.widjaja@here.com>
2016-07-11 21:41:44 +0200

    Render recommended books as default for now.

--- app/routes/books.routes.ts
+++ app/routes/books.routes.ts
@@ -3,6 +3,7 @@ import { BookListComponent }     from "../component/book-list";
 import { BookDetailComponent }   from "../component/book-detail";
 
 export const BooksRoutes: RouterConfig = [
-  { path: "books",  component: BookListComponent },
+  { path: "", redirectTo: "/books/recommended", pathMatch: "full" },
+  { path: "books/:category_id", component: BookListComponent },
   { path: "book/:id", component: BookDetailComponent }
 ];
